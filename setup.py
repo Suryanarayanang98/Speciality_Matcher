31c06551e8a1f328bfbe1b5dbc3cba48d3ec340b
Surya <suryanarayanang98@gmail.com>
2024-02-20 11:45:55 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Medical Speciality Standardiser and comparer'
+LONG_DESCRIPTION = 'A package that allows to standardise medical speciality & can be used for comparision.'
+
+# Setting up
 setup(
-    name='speciality-matcher',
-    version='1.0.0',
+    name="speciality-matcher",
+    version=VERSION,
+    author="None",
+    author_email="None",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=find_packages(),
-    package_data={'data': ['./data/nucc_taxonomy_mapper.csv']},
-    include_package_data=True,
-    install_requires=[
-        'fuzzywuzzy',
-        'pandas'
-    ],
+    install_requires=['fuzzywuzzy',
+        'pandas'],
+    keywords=['python', 'medical', 'speciality', 'fuzzy', 'standaridisation', 'comparision'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
